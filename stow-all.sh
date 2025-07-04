625c72b3f1f527a576a94568cb13832020d53665
mosahel01 <hoywd3465@gmail.com>
2025-07-04 11:50:18 +0530

    Updated

--- stow-all.sh
+++ stow-all.sh
@@ -6,6 +6,7 @@ TARGET=/home/mos
 cd "$DOTDIR" || exit 1
 
 # for pkg in bash bin awesome ghostty kitty nvim ; do
-for pkg in awesome bash bin ghostty kitty nvim zsh ; do
+# for pkg in awesome bash bin ghostty kitty nvim zsh ; do
+for pkg in tmux ; do
 	stow --dir="$DOTDIR" --target="$TARGET" "$pkg"
 done
