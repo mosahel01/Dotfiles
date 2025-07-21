6aa35fad2fac78f804f7cde563d5ba64ad4815de
mosahel01 <hoywd@gmail.com>
2025-07-21 10:43:33 +0530

    automation tool

--- stow-all.sh
+++ stow-all.sh
@@ -5,9 +5,7 @@ TARGET=$HOME
 
 cd "$DOTDIR" || exit 1
 
-# for pkg in bash bin awesome ghostty kitty nvim ; do
-# for pkg in awesome bash bin ghostty kitty nvim zsh ; do
-# for pkg in tmux ; do
-for pkg in awesome bash bin ghostty hypr kitty nvim rofi tmux waybar zed zsh ; do
+# for pkg in  alacritty bin ghostty hypr kitty nvim rofi tmux waybar zsh ; do
+for pkg in alacritty awesome bash bin ghostty hypr ideavimrc kitty nvim rofi tmux waybar zed zsh; do
 	stow --dir="$DOTDIR" --target="$TARGET" "$pkg"
 done
