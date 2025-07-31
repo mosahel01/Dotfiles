11899fb9fb73f4ebb3bea85733dccd94005400e6
mosahel <hoywd3465@gmail.com>
2025-07-31 21:47:11 +0530

    Fish shell

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,5 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     zoxide init fish | source
-    nitch
+    # nitch
 end
--- fish/.config/fish/functions/H.fish
+++
@@ -1,4 +0,0 @@
-function H --wraps='cd ~/.config/hypr/ && nvim .' --description 'alias H cd ~/.config/hypr/ && nvim .'
-  cd ~/.config/hypr/ && nvim . $argv
-        
-end
