ed3c8e3676d73b5fe9d193a6a49ac164df6ad8f5
mosahel <hoywd3465@gmail.com>
2025-08-09 18:26:39 +0530

    Nvim

--- nvim/.config/nvim/ftplugin/java.lua
+++ nvim/.config/nvim/ftplugin/java.lua
@@ -1,8 +1,6 @@
--- Minimal ftplugin/java.lua for kickstart.nvim + nvim-jdtls
-
 local ok, jdtls = pcall(require, 'jdtls')
 if not ok then
-  return
+    return
 end
 
 -- This workspace_dir must be unique per-project for full LSP power!
@@ -12,15 +10,15 @@ local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
 local workspace_dir = home .. '/jdtls_ws/' .. project_name
 
 jdtls.start_or_attach {
-  cmd = { 'jdtls' },
-  root_dir = require('jdtls.setup').find_root { '.git', 'mvnw', 'gradlew', 'pom.xml', 'build.gradle' },
-  workspace_folder = workspace_dir,
+    cmd = { 'jdtls' },
+    root_dir = require('jdtls.setup').find_root { '.git', 'mvnw', 'gradlew', 'pom.xml', 'build.gradle' },
+    workspace_folder = workspace_dir,
 }
 
--- Optional: Java-specific keymaps (for code actions, organize imports, etc.) -- Don't work
-vim.keymap.set('n', '<leader>oi', function()
-  vim.lsp.buf.code_action()
-end, { buffer = true, desc = 'Organize Imports' })
-vim.keymap.set('n', '<leader>rc', function()
-  vim.lsp.buf.rename()
-end, { buffer = true, desc = 'Rename Class' })
+-- -- Optional: Java-specific keymaps (for code actions, organize imports, etc.) -- Don't work
+-- vim.keymap.set('n', '<leader>oi', function()
+--     vim.lsp.buf.code_action()
+-- end, { buffer = true, desc = 'Organize Imports' })
+-- vim.keymap.set('n', '<leader>rc', function()
+--     vim.lsp.buf.rename()
+-- end, { buffer = true, desc = 'Rename Class' })
