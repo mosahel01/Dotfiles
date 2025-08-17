d569bb696475928c78999f252bd2a9ccf77e09e1
mosahel01 <hoywd3465@gmail.com>
2025-08-17 16:03:45 +0530

    Better than Home-Manager

--- stow-all.sh
+++ stow-all.sh
@@ -5,6 +5,7 @@ TARGET=$HOME
 
 cd "$DOTDIR" || exit 1
 
-for pkg in alacritty awesome bash bin fish ghostty hypr ideavimrc kitty nvim rofi starship tmux waybar zed zsh; do
+# for pkg in alacritty awesome bash bin fish ghostty hypr ideavimrc kitty nvim rofi starship tmux waybar zed zsh; do
+for pkg in alacritty bash hypr nvim rofi waybar; do
 	stow --dir="$DOTDIR" --target="$TARGET" "$pkg"
 done
