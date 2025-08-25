1f0b07323a54dbd715557bbc91427b16efb21656
mosahel01 <hoywd3465@gmail.com>
2025-08-25 11:49:17 +0530

    Backup removed

--- backup/.config/nvim/lua/mos/plugins/colorscheme.lua
+++
@@ -1,9 +0,0 @@
-return {
-    'doums/dark.nvim',
-    lazy = false,
-    priority = 1000,
-    config = function()
-        vim.cmd 'colorscheme dark'
-        -- require('dark').load() {} -- Disable setting bg (make neovim's background transparent)
-    end,
-}
