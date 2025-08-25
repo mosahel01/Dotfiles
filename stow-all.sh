728939101e2e1a8aeebf09d7091110a49b76e724
mosahel01 <hoywd3465@gmail.com>
2025-08-25 11:48:16 +0530

    Stow Script Updated

--- stow-all.sh
+++ stow-all.sh
@@ -6,6 +6,6 @@ TARGET=$HOME
 cd "$DOTDIR" || exit 1
 
 # for pkg in alacritty awesome bash bin fish ghostty hypr ideavimrc kitty nvim rofi starship tmux waybar zed zsh; do
-for pkg in alacritty bash hypr nvim rofi waybar; do
+for pkg in alacritty bash bin hypr nvim rofi tmux waybar; do
 	stow --dir="$DOTDIR" --target="$TARGET" "$pkg"
 done
