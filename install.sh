ce47aab27e5e3f68343a985fce8c055cc821c005
mosahel01 <mos.sahil01@gmail.com>
2026-07-26 12:34:03 +0530

    installer updated

--- install.sh
+++ install.sh
@@ -6,20 +6,10 @@ DOTFILES="$(cd "$(dirname "$0")" && pwd)"
 
 echo "Installing Dotfiles..."
 
-# # ghostty
-# mkdir -p "$HOME/.config/ghostty"
-# ln -sfn "$DOTFILES/config.ghostty" "$HOME/.config/ghostty/config"
-# echo "✓ symlinked ghostty"
-
 # # alacritty
 mkdir -p "$HOME/.config/alacritty"
 ln -sfn "$DOTFILES/alacritty.toml" "$HOME/.config/alacritty/alacritty.toml"
 echo "✓ symlinked alacritty"
-
-# # niri
-# mkdir -p "$HOME/.config/niri"
-# ln -sfn "$DOTFILES/config.niri" "$HOME/.config/niri/config.kdl"
-# echo "✓ symlinked niri"
 
 # zsh
 ln -sfn "$DOTFILES/zsh" "$HOME/.config/zsh"
@@ -28,10 +18,6 @@ echo "✓ symlinked zsh"
 # neovim
 ln -sfn "$DOTFILES/nvim" "$HOME/.config/nvim"
 echo "✓ symlinked neovim"
-
-# # zed editor
-# ln -sfn "$DOTFILES/zed" "$HOME/.config/zed"
-# echo "✓ symlinked zed editor"
 
 # fonts
 mkdir -p "$HOME/.config/fontconfig"
