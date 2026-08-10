ff191f05d463c5ccdf974556902b2617ccf08462
mosahel01 <mos.sahil01@gmail.com>
2026-08-10 09:34:12 +0530

    install: update setup script

--- install.sh
+++ install.sh
@@ -6,14 +6,22 @@ DOTFILES="$(cd "$(dirname "$0")" && pwd)"
 
 echo "Installing Dotfiles..."
 
-# # alacritty
+# alacritty
 mkdir -p "$HOME/.config/alacritty"
 ln -sfn "$DOTFILES/alacritty.toml" "$HOME/.config/alacritty/alacritty.toml"
 echo "✓ symlinked alacritty"
 
+# nvim
+ln -sfn "$DOTFILES/nvim" "$HOME/.config/nvim"
+echo "✓ symlinked nvim"
+
 # zsh
 ln -sfn "$DOTFILES/zsh" "$HOME/.config/zsh"
 echo "✓ symlinked zsh"
+
+# tmux
+ln -sfn "$DOTFILES/.tmux.conf" "$HOME/.tmux.conf"
+echo "✓ symlinked tmux"
 
 # fonts
 mkdir -p "$HOME/.config/fontconfig"
