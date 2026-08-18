eef84f4554218538e43d2b4f7183299f4881d546
mosahel01 <mos.sahil01@gmail.com>
2026-08-18 18:31:29 +0530

    neovim treesitter :(

--- nvim/lua/mosahil/plugins/treesitter.lua
+++ nvim/lua/mosahil/plugins/treesitter.lua
@@ -1,91 +1,76 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	event = { "BufReadPre", "BufNewFile" },
-	build = ":TSUpdate",
-	dependencies = {
-		"windwp/nvim-ts-autotag",
+	{
+		"nvim-treesitter/nvim-treesitter",
+		branch = "main",
+		event = { "BufReadPost", "BufNewFile" },
+		build = ":TSUpdate",
+		dependencies = {
+			"windwp/nvim-ts-autotag",
+		},
+		config = function()
+			require("nvim-ts-autotag").setup()
+
+			-- Map zsh to bash parser
+			vim.treesitter.language.register("bash", "zsh")
+
+			-- Native Neovim 0.12+ Highlighting & Syntax Auto-Start
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = "*",
+				callback = function(args)
+					-- Turn on built-in vim syntax fallback
+					vim.cmd("syntax on")
+					-- Start native Treesitter highlighting (fails silently if parser isn't installed)
+					pcall(vim.treesitter.start, args.buf)
+					-- Enable indentation
+					vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				end,
+			})
+		end,
 	},
-	config = function()
-		-- 1. Define your languages here
-		local parsers = {
-			"java",
-			"json",
-			"javascript",
-			"typescript",
-			"tsx",
-			"yaml",
-			"html",
-			"css",
-			"kotlin",
-			"c_sharp",
-			"prisma",
-			"markdown",
-			"ocaml",
-			"markdown_inline",
-			"swift",
-			"svelte",
-			"graphql",
-			"bash",
-			"go",
-			"lua",
-			"python",
-			"powershell",
-			"vim",
-			"dockerfile",
-			"gitignore",
-			"query",
-			"vimdoc",
-			"c",
-			"rust",
-		}
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		branch = "main",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		config = function()
+			local select = require("nvim-treesitter-textobjects.select")
 
-		-- 2. Automatically install any parsers that are missing
-		local already_installed = require("nvim-treesitter.config").get_installed()
-		local parsers_to_install = vim.iter(parsers)
-			:filter(function(p)
-				return not vim.tbl_contains(already_installed, p)
-			end)
-			:totable()
+			require("nvim-treesitter-textobjects").setup({
+				select = {
+					lookahead = true,
+				},
+			})
 
-		if #parsers_to_install > 0 then
-			require("nvim-treesitter").install(parsers_to_install)
-		end
+			-- Textobject keymaps (vif, vaf, vic, vac)
+			local keymaps = {
+				["if"] = "@function.inner",
+				["af"] = "@function.outer",
+				["ic"] = "@class.inner",
+				["ac"] = "@class.outer",
+				["ia"] = "@parameter.inner",
+				["aa"] = "@parameter.outer",
+			}
 
-		-- 3. Enable highlighting, indentation, and autotags natively
-		require("nvim-ts-autotag").setup()
-
-		vim.api.nvim_create_autocmd("FileType", {
-			callback = function()
-				pcall(vim.treesitter.start)
-				vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-			end,
-		})
-	end,
+			for key, query in pairs(keymaps) do
+				vim.keymap.set({ "x", "o" }, key, function()
+					select.select_textobject(query, "textobjects")
+				end, { desc = "Treesitter select " .. query })
+			end
+		end,
+	},
 }
 
 -- return {
 -- 	"nvim-treesitter/nvim-treesitter",
+-- 	branch = "master", -- Switches back to the classic framework branch
 -- 	event = { "BufReadPre", "BufNewFile" },
 -- 	build = ":TSUpdate",
 -- 	dependencies = {
 -- 		"windwp/nvim-ts-autotag",
 -- 	},
 -- 	config = function()
--- 		-- import nvim-treesitter plugin
--- 		-- local treesitter = require("nvim-treesitter")
+-- 		require("nvim-ts-autotag").setup()
 --
--- 		-- configure treesitter
--- 		require("nvim-treesitter.configs").setup({ -- enable syntax highlighting
--- 			highlight = {
--- 				enable = true,
--- 			},
--- 			-- enable indentation
--- 			indent = { enable = true },
--- 			-- enable autotagging (w/ nvim-ts-autotag plugin)
--- 			autotag = {
--- 				enable = true,
--- 			},
--- 			-- ensure these language parsers are installed
+-- 		require("nvim-treesitter").setup({
 -- 			ensure_installed = {
 -- 				"java",
 -- 				"json",
@@ -105,6 +90,8 @@ return {
 -- 				"svelte",
 -- 				"graphql",
 -- 				"bash",
+-- 				"sh",
+-- 				"zsh",
 -- 				"go",
 -- 				"lua",
 -- 				"python",
@@ -117,14 +104,13 @@ return {
 -- 				"c",
 -- 				"rust",
 -- 			},
--- 			incremental_selection = {
+-- 			sync_install = true,
+-- 			auto_install = true,
+-- 			highlight = {
 -- 				enable = true,
--- 				keymaps = {
--- 					init_selection = "<C-space>",
--- 					node_incremental = "<C-space>",
--- 					scope_incremental = false,
--- 					node_decremental = "<bs>",
--- 				},
+-- 			},
+-- 			indent = {
+-- 				enable = true,
 -- 			},
 -- 		})
 -- 	end,
